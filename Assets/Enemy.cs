e76fa5d2e43527ea6224eeb7e6a0314f9df8b931
Alex <alexander.richard96@gmail.com>
2016-01-10 22:47:01 -0400

    First commit might work maybe idfk

---
+++ Assets/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private Vector3 target;
+	private float speed = 2;
+	private float sight = 10;
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+		target = FindTarget ();
+
+		if (CanSeePlayer()) {
+			Chase ();
+		}
+	}
+
+	private Vector3 FindTarget() {
+
+		return target = GameObject.Find ("Player").transform.position;
+	}
+
+	private void Chase() {
+
+		float step = speed * Time.deltaTime;
+	
+		transform.LookAt(target);
+
+		gameObject.transform.position = Vector3.MoveTowards (transform.position, target, step);
+
+	}
+
+	private bool CanSeePlayer() {
+
+		if (Vector3.Distance (gameObject.transform.position, target) < sight) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	void OnTriggerEnter (Collider col) {
+
+		if (col.gameObject.tag == "weapon") {
+			Destroy (gameObject);
+		}
+
+	}
+}
