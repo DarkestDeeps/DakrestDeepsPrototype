f2c7261cd27b42fc8e1c3eb64b55e0c0632e68cc
Alex <alexander.richard96@gmail.com>
2016-01-12 17:05:16 -0400

    im gunna fuck the moon

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float movementSpeed = 10;
+	public float turningSpeed = 120;
+	private Animator anim;
+	private Rigidbody rigibody;
+	
+	void Start() {
+		anim = gameObject.GetComponent<Animator>();
+		rigibody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	void Update() {
+		
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		bool jump = Input.GetKeyDown ("space");
+		
+		transform.Rotate(0, h * turningSpeed * Time.deltaTime, 0);
+		
+		anim.SetInteger ("Direction", (int)v);
+		anim.SetBool ("Jump", jump);
+		anim.SetBool ("Grounded", isGrounded ());
+		
+	}
+
+	bool isGrounded() {
+		if (rigibody.velocity.y == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
