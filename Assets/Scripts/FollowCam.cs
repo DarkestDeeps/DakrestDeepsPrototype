159be740801ab0918921e0266f48eabfe08053fe
LombaxPieboy <Brandon.Ogon@gmail.com>
2016-01-14 21:27:02 -0400

    Yo. I changed the Stuff.
    Basically. It's changed. Hi 3d camera.

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -4,21 +4,56 @@ using System.Collections;
 public class FollowCam : MonoBehaviour {
 
 	public GameObject target;
-	public float damping = 1;
-	Vector3 offset;
+    public float rotateSpeed = 5;
+    public float xSpeed = 115.0f;
+    public float ySpeed = 50.0f;
+	public float distance;
+    public float distanceMin = 0.5f;
+
+    private Vector3 offset;
+    private float x;
+    private float y;
+    private bool eyy;
+    private float dist = 5.0f;
 	
 	void Start() {
-		offset = target.transform.position - transform.position;
+        offset = new Vector3(target.transform.position.x, target.transform.position.y-15, target.transform.position.z+10);
+        transform.LookAt(target.transform);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        eyy = true;
 	}
 	
 	void LateUpdate() {
-		float currentAngle = transform.eulerAngles.y;
-		float desiredAngle = target.transform.eulerAngles.y;
-		float angle = Mathf.LerpAngle(currentAngle, desiredAngle, damping);
-		
-		Quaternion rotation = Quaternion.Euler(0, angle, 0);
-		transform.position = target.transform.position - (rotation * offset);
-		
-		transform.LookAt(target.transform);
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (eyy == true) {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                eyy = false;
+            } else {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                eyy = true;
+            }
+        }
+
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+        //y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+        dist = Mathf.Clamp(dist - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distance);
+
+        RaycastHit hit;
+        if (Physics.Linecast(target.transform.position, transform.position, out hit)) {
+            dist -= hit.distance;
+        }
+        Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.transform.position;
+
+        transform.rotation = rotation;
+        transform.position = position;
 	}
 }
